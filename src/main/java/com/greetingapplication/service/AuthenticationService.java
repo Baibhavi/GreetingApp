b0e3dc0674e7f950765114b5264260734445cbe7
Baibhavi <baibhavipandey@gmail.com>
2025-03-05 12:01:14 +0530

    Added: Bcrypt and Swagger

--- src/main/java/com/greetingapplication/service/AuthenticationService.java
+++ src/main/java/com/greetingapplication/service/AuthenticationService.java
@@ -15,6 +15,7 @@ import java.util.Optional;
 
 @Service
 public class AuthenticationService implements IAuthenticationService {
+
     @Autowired
     AuthUserRepository authUserRepository;
 
@@ -27,28 +28,40 @@ public class AuthenticationService implements IAuthenticationService {
     BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
     @Override
-    public AuthUser register(AuthUserDTO userDTO) throws Exception{
-        AuthUser user=new AuthUser(userDTO);
-        System.out.println(user);
-        String token=tokenUtil.createToken(user.getUserId());
+    public AuthUser register(AuthUserDTO userDTO) throws Exception {
+        AuthUser user = new AuthUser(userDTO);
+
+        String encryptedPassword = passwordEncoder.encode(userDTO.getPassword());
+        user.setPassword(encryptedPassword);
+
+        String token = tokenUtil.createToken(user.getUserId());
         authUserRepository.save(user);
-        emailSenderService.sendEmail(user.getEmail(),"Registered in Greeting App", "Hii...."
-                +user.getFirstName()+"\n You have been successfully registered!\n\n Your registered details are:\n\n User Id:  "
-                +user.getUserId()+"\n First Name:  "
-                +user.getFirstName()+"\n Last Name:  "
-                +user.getLastName()+"\n Email:  "
-                +user.getEmail()+"\n Address:  "
-                +"\n Token:  " +token);
+
+        emailSenderService.sendEmail(user.getEmail(), "Registered in Greeting App", "Hi "
+                + user.getFirstName() + ",\nYou have been successfully registered!\n\nYour registered details are:\n\n User Id:  "
+                + user.getUserId() + "\n First Name:  "
+                + user.getFirstName() + "\n Last Name:  "
+                + user.getLastName() + "\n Email:  "
+                + user.getEmail() + "\n Token:  " + token);
+
         return user;
     }
 
     @Override
-    public String login(LoginDTO loginDTO){
-        Optional<AuthUser> user= Optional.ofNullable(authUserRepository.findByEmail(loginDTO.getEmail()));
-        if (user.isPresent() && user.get().getPassword().equals(loginDTO.getPassword()) ){
-            emailSenderService.sendEmail(user.get().getEmail(),"Logged in Successfully!", "Hii...."+user.get().getFirstName()+"\n\n You have successfully logged in into Greeting App!");
-            return "Congratulations!! You have logged in successfully!";
-        }else {
+    public String login(LoginDTO loginDTO) {
+        Optional<AuthUser> user = Optional.ofNullable(authUserRepository.findByEmail(loginDTO.getEmail()));
+
+        if (user.isPresent()) {
+            // Check if the password matches the encrypted password
+            if (passwordEncoder.matches(loginDTO.getPassword(), user.get().getPassword())) {
+                emailSenderService.sendEmail(user.get().getEmail(), "Logged in Successfully!", "Hi "
+                        + user.get().getFirstName() + ",\n\nYou have successfully logged in into Greeting App!");
+
+                return "Congratulations!! You have logged in successfully!";
+            } else {
+                throw new UserException("Sorry! Email or Password is incorrect!");
+            }
+        } else {
             throw new UserException("Sorry! Email or Password is incorrect!");
         }
     }
